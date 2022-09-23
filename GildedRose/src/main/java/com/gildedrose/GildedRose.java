85dfee2f001d18aa88f8d716829928a058b02d25
mathieu <mathieu.barton@etu.uca.fr>
2022-09-23 15:03:51 +0200

    Pitest 100%

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -3,7 +3,7 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    GildedRose(Item[] items) {
         this.items = items;
     }
 
--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -41,6 +41,15 @@ class GildedRoseTest {
     assertThat(element.quality, is(0));
   }
 
+  @Test
+  @DisplayName("test lambda item for quality reduction sellin past")
+  void testLambdaQualityReductionSellinaAt0() {
+    Item element = new Item("foo", 1, 2);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertThat(element.quality, is(1));
+  }
+
 
   @Test
   @DisplayName("test aged brie item for quality augmentation")
@@ -108,6 +117,14 @@ class GildedRoseTest {
   }
 
   @Test
+  @DisplayName("test Backstage passes to a TAFKAL80ETC concert item for quality augmentation 11 day before ")
+  void testBackstage11DayBefore() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 2);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertThat(element.quality, is(3));
+  }
+  @Test
   @DisplayName("test Backstage passes to a TAFKAL80ETC concert item for quality augmentation 10 day before ")
   void testBackstage10DayBefore() {
     Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 2);
@@ -124,6 +141,14 @@ class GildedRoseTest {
     assertThat(element.quality, is(50));
   }
 
+  @Test
+  @DisplayName("test Backstage passes to a TAFKAL80ETC concert item for quality augmentation 6 day before ")
+  void testBackstage6DayBefore() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 2);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertThat(element.quality, is(4));
+  }
   @Test
   @DisplayName("test Backstage passes to a TAFKAL80ETC concert item for quality augmentation 5 day before ")
   void testBackstage5DayBefore() {
